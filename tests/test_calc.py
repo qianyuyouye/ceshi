02cfb66eb15d9d4105e6e34d823f50027eb204fa
qianyuyouye <alvarodirgantara08@gmail.com>
2026-09-24 23:08:57 +0800

    autofix: ch-wave-7e468c363af3

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,7 +1,13 @@
-from src.calc import add
+from src.calc import add, sub
 
 
 def test_add():
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
+
+def test_sub():
+    assert sub(3, 2) == 1
+    assert sub(1, 2) == -1
+    assert sub(0, 0) == 0
